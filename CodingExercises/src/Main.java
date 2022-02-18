885bd05320ccb1291b68aeffe86d435ecbefd65f
SANATBEK MATLATIPOV <mr.sanatbek@gmail.com>
2022-02-18 18:54:27 +0500

    Main java file is added. PerfectNumber got finished

---
+++ CodingExercises/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----- Ex 22: Perfect Number -----");
+        System.out.println(PerfectNumber.isPerfectNumber(6));
+        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println(PerfectNumber.isPerfectNumber(5));
+        System.out.println(PerfectNumber.isPerfectNumber(-1));
+        System.out.println("----- END -----");
+
+    }
+}
