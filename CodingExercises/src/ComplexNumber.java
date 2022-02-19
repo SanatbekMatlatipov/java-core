95759afced407f551321f5fe1f99f77a98a45174
SANATBEK MATLATIPOV <mr.sanatbek@gmail.com>
2022-02-19 12:56:17 +0500

    Exercise 33 and 34 are just finsihed

---
+++ CodingExercises/src/ComplexNumber.java
@@ -0,0 +1,27 @@
+public class ComplexOperations {
+    private double real;
+    private double imaginary;
+
+    public ComplexOperations(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(Co) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+}
