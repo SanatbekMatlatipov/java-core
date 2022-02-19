50290847a13ef01175cb8da5b238082183a4e962
SANATBEK MATLATIPOV <mr.sanatbek@gmail.com>
2022-02-19 11:20:29 +0500

    Classes part-1, part-2 and constructor part-1 lecture with challenges is finished

---
+++ Classes/src/uz/sanatbek/Account.java
@@ -0,0 +1,53 @@
+package uz.sanatbek;
+
+public class Bank {
+    int accountNumber;
+    double balance;
+    String customerName;
+    String email;
+    String phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void topUpBalance(double deposit) {
+        this.balance += deposit;
+    }
+    
+}
