ab5074678f64737f84815aab69e3c7d478e61a62
SANATBEK MATLATIPOV <mr.sanatbek@gmail.com>
2022-02-21 07:16:09 +0500

    LinkedList is finished

--- Arrays/src/Challenge/Challenge.java
+++
@@ -1,2 +0,0 @@
-package Challenge;public class Challenge {
-}
---
+++ Arrays/src/Challenge/Main.java
@@ -0,0 +1,12 @@
+package Challenge;
+
+import java.util.ArrayList;
+
+public class PlayList {
+    ArrayList<Album> albumsList;
+
+    public PlayList(ArrayList<Album> albumsList) {
+        this.albumsList = albumsList;
+    }
+
+}
