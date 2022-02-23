37ae47f0d6cae6e03d77e7b03d73f8d6a12a3890
SANATBEK MATLATIPOV <mr.sanatbek@gmail.com>
2022-02-23 13:26:19 +0500

    Lambda

--- Lambda/src/Main.java
+++ Lambda/src/Main.java
@@ -1,2 +1,83 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim  Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(tim);
+
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
+
+class AnotherClass {
+    public String doSomething() {
+        final int i = 1;
+        UpperConcat uc = (s1, s2) -> {
+            System.out.println("Lambda expressions class is" + getClass().getSimpleName());
+            System.out.println("i int the lambda expressions = " + i);
+            String result = s1.toUpperCase() + s2.toUpperCase();
+            return result;
+        };
+
+        System.out.println("AnotherClass class's name is " + getClass().getSimpleName());
+        return Main.doStringStuff(uc, "String1", "String2");
+    }
+
+    public void printValue() {
+        int number = 25;
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("The value is " + number);
+        };
+        new Thread(runnable).start();
+    }
 }
