001a20c3801ee7eea60e5ccb3583592cfd3a963a
sanatbek-wb <mr.sanatbek@gmail.com>
2024-12-27 12:26:05 +0500

    OOP Object theory observed

---
+++ section7inheritance/object/Main.java
@@ -0,0 +1,45 @@
+package section7inheritance.object;
+
+public class Main {
+    public static void main(String[] args) {
+        Student max = new Student("Max", 21);
+        System.out.println(max);
+
+        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 8, "Carole");
+        System.out.println(jimmy);
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class PrimarySchoolStudent extends Student {
+    private String parentName;
+
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return "PrimarySchoolStudent{" +
+                "parentName='" + parentName + '\'' +
+                "} " + super.toString();
+    }
+}
